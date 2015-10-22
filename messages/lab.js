da6bea83ba2983258b93057db8bda97d9ad322c3
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2015-10-22 19:09:48 -0400

    changed js to handle message not found

--- messages/lab.js
+++ messages/lab.js
@@ -5,7 +5,7 @@ function parse() {
 	request.open("GET", "data.json", true);
 	request.send(); 
 	request.onreadystatechange = function() {
-		if (request.readyState == 4) {
+		if (request.readyState == 4 && request.status == 200) {
 			var data = request.responseText;
 			data = JSON.parse(data);
 			elem = document.getElementById("messages");
@@ -14,6 +14,12 @@ function parse() {
 				content += "<p><span class='content'>" + data[i]['content'] + "</span><br>" + data[i]['username'] + "</p>";
 			}
 			elem.innerHTML = content;
+		} else if (request.readyState == 4 && request.status != 200) {
+			elem = document.getElementById("messages");
+			elem.innerHTML = "<p>Messages Not Found</p>";
+		} else {
+			elem = document.getElementById("messages");
+			elem.innerHTML = "<p>Looking...</p>"
 		}
 	}
 }
